d286584f4f56e7590a7e4869ae738bc62d640464
Bruno Antonio Monzón <brunomonzon2002@gmail.com>
2026-05-13 16:59:50 -0400

    feat: actualiza health check a version 2.0.0

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const libros = require('./libros');
 app.use(express.json());
 
 app.get('/health', (req, res) => {
-  res.status(200).json({ status: 'ok', service: 'crud-libros' });
+  res.status(200).json({ status: 'ok', service: 'crud-libros', version: '2.0.0' });
 });
 
 app.get('/libros', (req, res) => {
